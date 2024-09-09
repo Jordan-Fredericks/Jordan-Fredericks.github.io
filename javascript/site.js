0318705491ba53f0ab482511ba690dbc6c8f1d2c
Fredericks <500172971@fvtc.edu>
2024-09-09 14:57:50 -0500

    Added js to test js file linking

--- javascript/site.js
+++ javascript/site.js
@@ -1,9 +1,10 @@
-// function test(event)
-// {
-//     const html = document.querySelector('html')
-//     html.style.backgroundColor = 'white'
-//     back.style.backgroundColor = 'white'
-// }
+function test(event)
+{
+    // const html = document.querySelector('html')
+    // html.style.backgroundColor = 'white'
+    // back.style.backgroundColor = 'white'
+    console.log(event.target.closest ('body'))
+}
 
-// const back = document.querySelector('body')
-// back.addEventListener('click', test)
+const back = document.querySelector('body')
+back.addEventListener('click', test)
