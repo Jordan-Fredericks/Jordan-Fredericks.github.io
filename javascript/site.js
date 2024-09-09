b36bcd89ec697f3dcb786b29c366530afa0f1690
Fredericks <500172971@fvtc.edu>
2024-09-09 14:47:34 -0500

    Added short bio

--- javascript/site.js
+++ javascript/site.js
@@ -1,7 +1,5 @@
 function test(event)
 {
-    //console.log(event.target.parentElement)
-    //console.log(event.target.closest ('body'))
     const html = document.querySelector('html')
     html.style.backgroundColor = 'white'
     back.style.backgroundColor = 'white'
