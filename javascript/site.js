4636c89a666333e0f32f0a068252f0cf9b692b44
Fredericks <500172971@fvtc.edu>
2024-09-09 15:02:31 -0500

    Testing js

--- javascript/site.js
+++ javascript/site.js
@@ -1,8 +1,8 @@
 function test(event)
 {
-    // const html = document.querySelector('html')
-    // html.style.backgroundColor = 'white'
-    // back.style.backgroundColor = 'white'
+    const html = document.querySelector('html')
+    html.style.backgroundColor = 'white'
+    back.style.backgroundColor = 'white'
     console.log(event.target.closest ('body'))
 }
 
