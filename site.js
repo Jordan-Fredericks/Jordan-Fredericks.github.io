933d47b12369bca5e3469e5db99274b77ff09058
Fredericks <500172971@fvtc.edu>
2024-09-09 15:09:49 -0500

    Removed back changer

--- site.js
+++ site.js
@@ -1,8 +1,5 @@
 function test(event)
 {
-    // const html = document.querySelector('html')
-    // html.style.backgroundColor = 'white'
-    // back.style.backgroundColor = 'white'
     console.log(event.target.closest ('body'))
 }
 
