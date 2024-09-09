f909c235d62ce243e2dc3e1bb5a641bf1342062f
Fredericks <500172971@fvtc.edu>
2024-09-09 14:53:26 -0500

    Added favorite's list and removed js

--- javascript/site.js
+++ javascript/site.js
@@ -1,9 +1,9 @@
-function test(event)
-{
-    const html = document.querySelector('html')
-    html.style.backgroundColor = 'white'
-    back.style.backgroundColor = 'white'
-}
+// function test(event)
+// {
+//     const html = document.querySelector('html')
+//     html.style.backgroundColor = 'white'
+//     back.style.backgroundColor = 'white'
+// }
 
-const back = document.querySelector('body')
-back.addEventListener('click', test)
+// const back = document.querySelector('body')
+// back.addEventListener('click', test)
