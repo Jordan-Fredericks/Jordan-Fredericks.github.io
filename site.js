e0a124d5a6644677d43f26038d397dbb369b4ad7
Fredericks <500172971@fvtc.edu>
2024-09-30 14:38:21 -0500

    Added carousel

--- site.js
+++ site.js
@@ -1,10 +1,12 @@
-function test(event)
-{
-    console.log(event.target.closest ('body'))
-}
+// JS Script linking test code
 
-const back = document.querySelector('body')
-back.addEventListener('click', test)
+// function test(event)
+// {
+//     console.log(event.target.closest ('body'))
+// }
+
+// const back = document.querySelector('body')
+// back.addEventListener('click', test)
 
 const hours = new Date().getHours() // get the current hour
 
@@ -22,3 +24,42 @@ else
     welcome.textContent = 'Good Evening'
 
 localStorage.setItem("It's a secret to everybody.", 'I really hope this is a secret message that no-one can see.')
+
+//Assignment 5 Code
+const urls = [
+    'https://images.pexels.com/photos/1454360/pexels-photo-1454360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/933964/pexels-photo-933964.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1251861/pexels-photo-1251861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1370296/pexels-photo-1370296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+].map(url => { (new Image()).src = url; return url })
+
+const images = document.querySelectorAll('#carousel img')
+
+let currentImage = 0
+const showImages = () => {
+    const offset = currentImage % urls.length
+    images.forEach((image, index) => {
+        const imageIndex = (index + offset + urls.length) % urls.length
+        image.src = urls[imageIndex]
+    })
+}
+
+showImages()
+
+const prevButton = document.querySelector('#prev')
+const nextButton = document.querySelector('#next')
+
+prevButton.addEventListener('click', () => {
+    currentImage--
+    showImages()
+})
+nextButton.addEventListener('click', () => {
+    currentImage++
+    showImages()
+})
+
+setInterval(() => {
+    currentImage++
+    showImages()
+}, 5000);
